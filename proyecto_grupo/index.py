4b3d38025d69349c1263a7a76c30586e102ffcb5
jpguerra <guerracadenaj@gmail.com>
2024-09-04 06:28:02 -0500

    ADD-JPGC: Se carga todo el proyecto para el debido seguimiento y para repartir los aspectos de la fase cocreacon 2.

---
+++ proyecto_grupo/index.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+# Objeto principal de Flask para manejar la aplicación web
+app = Flask(__name__)
+
+# Definición de las rutas
+@app.route('/')
+def principal():
+    return render_template('index.html')
+
+@app.route('/boyaca')
+def boyaca():
+    return render_template('Boyaca.html')
+
+@app.route('/cundi')
+def cundi():
+    return render_template('cundi.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('Contacto.html')
+
+# Levantar la Aplicación WEB para observarla en el Navegador
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
